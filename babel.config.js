108c5007c422a5d57d97fdae6cbed60ed36897b8
Christian Montoya <christian.montoya@sap.com>
2021-08-19 13:48:34 -0400

    Integrate Unit Testing

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
 /* eslint-env node */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import * as fs from 'fs-extra'
+let extend
+
+/**
+ * The .babelrc file has been created to assist Jest for transpiling.
+ * You should keep your application's babel rules in this file.
+ */
+
+if (fs.existsSync('./.babelrc')) {
+    extend = './.babelrc'
+}
+
 module.exports = {
-  presets: [
-    '@quasar/babel-preset-app'
-  ]
+    presets: ['@quasar/babel-preset-app', '@babel/preset-typescript'],
+    extends: extend
 }
